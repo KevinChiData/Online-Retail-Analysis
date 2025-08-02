d63e174b4f27bba070071c45a2398605fed66a62
KevinChiData <kevinchi164@gmail.com>
2025-08-01 20:30:48 -0700

    Created revenue plot

--- scripts/convert_to_sql.py
+++ scripts/convert_to_sql.py
@@ -1,13 +1,24 @@
 import pandas as pd
 from sqlalchemy import create_engine
 
+# Read and save csv to df
 df = pd.read_csv('data/online_retail.csv')
 
+# Clean data by removing null values
 df = df.dropna(subset=['InvoiceNo', 'CustomerID'])
+
+# Convert InvoiceDate to datetime format
 df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
+
+# Remove negative quantity and price values
 df = df[(df['Quantity'] > 0) & (df['UnitPrice'] > 0)]
+
+# Create Revenue column
 df['Revenue'] = df['UnitPrice'] * df['Quantity']
+
+# Convert CustomerID to string
 df['CustomerID'] = df['CustomerID'].astype(str)
 
+# Create database to create SQL queries
 engine = create_engine('sqlite:///data/online_retail_sql.db')
 df.to_sql('online_retail_sql', con=engine, if_exists='replace', index=False)
