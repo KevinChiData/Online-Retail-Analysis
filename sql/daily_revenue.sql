426cf38665dea854895e25e3f5a05744d4f8a682
KevinChiData <kevinchi164@gmail.com>
2025-08-09 00:57:20 -0700

    Created pngs and csvs of outputs

--- sql/daily_revenue.sql
+++ sql/daily_revenue.sql
@@ -1,2 +1,6 @@
-SELECT CAST(InvoiceDate AS DATE), Quantity, UnitPrice
+SELECT STRFTIME('%Y-%M', InvoiceDate) AS YearMonth,
+    ROUND(SUM(Quantity), 2) AS TotalQuantity,
+    ROUND(SUM(Revenue), 2) AS TotalRevenue
 FROM online_retail_sql
+GROUP BY YearMonth
+ORDER BY YearMonth
